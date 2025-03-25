7f1fa6f2e9f5ec63ff0f81e1b74df353d8087e5c
UlisesHdz <ulisesdejesusk9999@gmail.com>
2025-03-25 09:31:18 -0600

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import "./App.css";
-import { Menu } from "./componens/Menu";
 
 function App() {
-  const eventoClick = () => {
-    console.log("precionado");
+  const [number, setNumber] = useState(0);
+
+  const handleClickContador = () => {
+    setNumber(number + 1);
+    console.log("value", number);
   };
+
   return (
     <>
-      <h1 onClick={eventoClick}>dame click</h1>
+      <h1 onClick={handleClickContador}>contador {number}</h1>
+      <button onClick={handleClickContador}>Incrementar</button>
     </>
   );
 }
