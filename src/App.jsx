e9144a02466677ae5c6ae1de0db14dda5738704e
UlisesHdz <ulisesdejesusk9999@gmail.com>
2025-03-25 13:56:18 -0600

    Renderizado de listas de Objetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 import "./App.css";
+import { animals } from "./data/animals";
+
+const HTMLanimmals = animals.map((animal) => {
+  return (
+    <li key={animal.id}>
+      <img src={animal.img} alt={animal.name} width={200} />
+      <h3>{animal.name}</h3>
+    </li>
+  );
+});
 
 function App() {
-  const movies = ["lord of the rings", "the hobbit", "the lord of the rings"];
-
-  const HTMLmovies = movies.map((movie) => <li key={movie}>{movie}</li>);
-
   return (
     <>
-      <h1>Renderizado de listas</h1>
-      <ul>{HTMLmovies}</ul>
+      <h1>Renderizado de listas de objetos</h1>
 
+      <ul>{HTMLanimmals}</ul>
+
+      <p>la otra forma seria</p>
       <br />
-      <h1>otra forma</h1>
-
       <ul>
-        {movies.map((movie) => (
-          <li key={movie}>{movie}</li>
-        ))}
+        {animals.map((animal) => {
+          return (
+            <li key={animal.id}>
+              <img src={animal.img} alt={animal.name} width={200} />
+              <h3>{animal.name}</h3>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
