f28735a80d5a6fde32c731e13a63572228a58420
UlisesHdz <ulisesdejesusk9999@gmail.com>
2025-03-25 12:40:56 -0600

    Renderizado condicional

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,19 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [myText, setMyText] = useState("Tu Nombre");
-  const [myValue, setMyValue] = useState("");
-
-  const [num1, setNum1] = useState("");
-  const [num2, setNum2] = useState("");
-  const [sum, setSum] = useState(0);
-
-  const handleInput = (e) => {
-    console.log(e.target.value);
-    setMyValue(e.target.value);
-  };
-
-  const handleInputN1 = (e) => {
-    setNum1(e.target.value);
-  };
-
-  const handleInputN2 = (e) => {
-    setNum2(e.target.value);
-  };
-
-  const handleSum = () => {
-    setSum(Number(num1) + Number(num2)); // Convertimos a número antes de sumar
-  };
+  const condition = false;
 
   return (
     <>
-      <h1>hola a todos</h1>
-      <h2>{myValue}</h2>
-      <input
-        type="text"
-        placeholder={myText}
-        value={myValue}
-        onChange={handleInput}
-      />
+      <h1>Renderizado condicional</h1>
+      {condition && <h1>Esto es visible</h1>}
+      {!condition && <h1>Esto no es visible</h1>}
 
-      <br />
-      <h2>es para la suma de dos numeros : {sum}</h2>
-      <input
-        type="number"
-        placeholder="Ingrese el primer numero"
-        value={num1}
-        onChange={handleInputN1}
-      />
 
-      <input
-        type="number"
-        placeholder="Ingrese el segundo numero"
-        value={num2}
-        onChange={handleInputN2}
-      />
-
-      <button onClick={handleSum}>Sumar</button>
-    </>
+      {/* {condition ? <h1>Esto es visible</h1> : <h1>Esto no es visible</h1>}
+     */}
+     
+     </>
   );
 }
 
