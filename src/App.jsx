f94ff5dfbfc1c0ae5cd60c929c9f3bb4cfb6b2d2
UlisesHdz <ulisesdejesusk9999@gmail.com>
2025-03-26 16:03:30 -0600

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,13 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { BlogPage } from "./pages/BlogPage";
 
 function App() {
-  const [user, setUser] = useState({
-    userName: "",
-    password: "",
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(user);
-    console.log("enviado");
-  };
-
-  const handleUserNameInput = (e) => {
-    setUser({ ...user, userName: e.target.value });
-  };
-
-  const handlePasswordInput = (e) => {
-    setUser({ ...user, password: e.target.value });
-  };
-
   return (
-    <div onSubmit={handleSubmit}>
-      <h1>Formularios</h1>
-      <form>
-        <fieldset>
-          <label htmlFor="userName">Nombre</label>
-          <input
-            type="text"
-            id="userName"
-            onChange={handleUserNameInput}
-            value={user.userName}
-          />
-        </fieldset>
-
-        <fieldset>
-          <label htmlFor="password">Contraseña</label>
-          <input
-            type="password"
-            id="password"
-            onChange={handlePasswordInput}
-            value={user.password}
-          />
-        </fieldset>
-
-        <button>Enviar</button>
-      </form>
-      <button onClick={() => setUser({ userName: "", password: "" })}>
-        logout
-      </button>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/blog" element={<BlogPage/>} />
+    </Routes>
   );
 }
 
